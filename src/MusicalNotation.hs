d07b635f1097a893cc80c5e57c2e6238c0eb8732
Mateu Adsuara <mateuadsuara@gmail.com>
2020-10-04 01:33:54 +0200

    Test MusicalNotation two notes

--- src/MusicalNotation.hs
+++ src/MusicalNotation.hs
@@ -25,11 +25,14 @@ readInput input = (amount, notes)
         readNote (pitch:[])       = (pitch, 1)
         readNote (pitch:duration) = (pitch, (read duration)::Int)
 
-formatNotes _ [] = []
+formatNotes _ [] = ""
 formatNotes line@(pitchInLine, separator) ((pitch, duration):remainingNotes) =
-  noteStr ++ remainingStr
+  noteStr ++ separatorStr ++ remainingStr
   where noteChar = if pitchInLine == pitch then '*' else separator
         noteStr = replicate duration noteChar
+        separatorStr = if null remainingNotes
+          then ""
+          else [separator]
         remainingStr = formatNotes line remainingNotes
 
 io input = intercalate "\n" (map formatLine staffLines)
--- test/MusicalNotationSpec.hs
+++ test/MusicalNotationSpec.hs
@@ -59,3 +59,21 @@ spec = do
          \c:   \n\
          \b:   \n\
          \a: --"
+    it "two notes" $ do
+      io "2\n\
+         \G F2"
+      `shouldBe`
+         "G: *   \n\
+         \F: --**\n\
+         \E:     \n\
+         \D: ----\n\
+         \C:     \n\
+         \B: ----\n\
+         \A:     \n\
+         \g: ----\n\
+         \f:     \n\
+         \e: ----\n\
+         \d:     \n\
+         \c:     \n\
+         \b:     \n\
+         \a: ----"
